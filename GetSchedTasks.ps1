017e379952b4efc8f298707a2568eaffbfa391f3
Brian Dill <brian.dill@randasolutions.com>
2021-07-04 21:23:24 -0500

    now able to specify multiple task folders.  Added comments.

--- GetSchedTasks.ps1
+++ GetSchedTasks.ps1
@@ -1,16 +1,19 @@
 ﻿# Desc: Lists all scheduled tasks and important properties
 # Auth: Brian Dill 2021-07-03
-# borrowed from: https://stackoverflow.com/questions/32731366/how-to-get-trigger-details-associated-with-a-task-in-task-scheduler-from-powersh
+# URL:  https://github.com/wbdill/PowerShell-sandbox01/blob/master/GetSchedTasks.ps1
+# Inspiration:  https://stackoverflow.com/questions/32731366/how-to-get-trigger-details-associated-with-a-task-in-task-scheduler-from-powersh
+# Dependencies: https://dbatools.io/   # Install-Module -Name dbatools  # (must run Powershell as admin to install module)
 
 Clear-Host
 #------------------------------------------------------------------------------------------
 # Config vars
 #------------------------------------------------------------------------------------------
-$TaskNameLike   = "*"          # "*" to get all tasks.        "Foo*" to get tasks that start with "Foo"
-$TaskFolder     = "\"          # "\" = tasks in root folder.  "\TOWER\" = all tasks in TOWER folder
-$SaveToCsv      = 1            # 1 to save, 0 to n\ot save
-$CsvSaveFolder  = "C:\Tasks\"  # Where you want to save the CSV output
+$TaskNameLike   = "*"              # "*" to get all tasks.     "Foo*" to get tasks that start with "Foo"
+$TaskFolders    = "\", "\TOWER\"   # String array of folders.  "\" = tasks in root folder.  "\Bar\" = all tasks in Bar folder
+$SaveToCsv      = 1                # 1 to save, 0 to n\ot save
+$CsvSaveFolder  = "C:\Tasks\"      # Where you want to save the CSV output
 
+$TasksResult = @()                 # var to hold results
 #------------------------------------------------------------------------------------------
 Function BinaryWeekdaysToString {
     param([int]$DaysOfWeek) 
@@ -31,70 +34,75 @@ Function BinaryWeekdaysToString {
     Return $output.TrimEnd(",")
 }
 #------------------------------------------------------------------------------------------
-$Result = @()
-Get-ScheduledTask -TaskPath $TaskFolder | 
-    Where-Object { $_.TaskName-like $TaskNameLike } | ForEach-Object {
+$TaskFolders | ForEach-Object {
+    $TaskFolder= $_
+    Get-ScheduledTask -TaskPath $TaskFolder | 
+        Where-Object { $_.TaskName-like $TaskNameLike } | ForEach-Object {
 
-    $Task = $_
-    Write-Host $Task.TaskName
-    Try {
-        [string]$Name          = $Task.TaskName
-        [string]$Author        = $Task.Author
-        [string]$Description   = $Task.Description
-        [bool]$Enabled         = $Task.Settings.Enabled
-        [string]$Action        = $Task.Actions | Select -ExpandProperty Execute
-        [string]$Arguments     = $Task.Actions | Select -ExpandProperty Arguments
-
-        # Init vars in case no trigger
-        [datetime]$Start       = (Get-Date -Month 1 -Day 1 -Year 1900 -Hour 0 -Minute 0 -Second 0) # bogus 1900-01-01 date
-        [string]$StartTime     = ""
-        [string]$Repetition    = "No Trigger"
-        [string]$Duration      = "No Trigger"
-        [string]$TriggerDays   = "No Trigger"
-        [int]$DaysOfWeek       = -1  # -1 to indicate no trigger
-
+        $Task = $_
+        Write-Host $Task.TaskName
         Try {
-            # If the task has a triger, overwrite the trigger vars with valid values
-            $Start         = $Task.Triggers | Select -ExpandProperty StartBoundary
-            $StartTime     = $Start.ToString("hh:mm")
-            $DaysOfWeek    = $Task.Triggers | Select -Expandproperty DaysOfWeek       #$Task.Triggers[0].DaysOfWeek 
-            $Repetition    = $Task.Triggers.Repetition | Select -ExpandProperty Interval
-            $Duration      = $Task.triggers.Repetition | Select -ExpandProperty Duration
-            $TriggerDays   = $Task.Triggers | Select -ExpandProperty DaysInterval  #$Task.Triggers[0].DaysInterval
-        } Catch {
-            Write-Host "--- INFO: No Trigger found for $Name"
-        }
-            
-        $splat = @{
-            'Name'        = $Name
-            'Author'      = $Author
-            'Description' = $Description
-            'Action'      = $Action
-            'Arguments'   = $Arguments
-            'Start'       = $Start
-            'StartTime'   = $StartTime
-            'Duration'    = $Duration
-            'Repetition'  = $Repetition
-            'DaysOfWeek'  = BinaryWeekdaysToString $DaysOfWeek
-            'TriggerDays' = If ($TriggerDays -eq 1) { "Daily" } else { $TriggerDays }
-            'Enabled'     = If ($Enabled) { "Enabled" } Else { "-" }
-        }
+            [string]$Name          = $Task.TaskName
+            [string]$Author        = $Task.Author
+            [string]$Description   = $Task.Description
+            [bool]$Enabled         = $Task.Settings.Enabled
+            [string]$Action        = $Task.Actions | Select -ExpandProperty Execute
+            [string]$Arguments     = $Task.Actions | Select -ExpandProperty Arguments
+            [string]$TaskPath      = $Task.TaskPath
 
-        $obj = New-Object -TypeName PSObject -property $splat
-        $Result += $obj
-    }
-    Catch {
-        Write-Host "ERROR: $Error"
+            # Init vars in case no trigger
+            [datetime]$Start       = (Get-Date -Month 1 -Day 1 -Year 1900 -Hour 0 -Minute 0 -Second 0) # bogus 1900-01-01 date
+            [string]$StartTime     = ""
+            [string]$Repetition    = "No Trigger"
+            [string]$Duration      = "No Trigger"
+            [string]$TriggerDays   = "No Trigger"
+            [int]$DaysOfWeek       = -1  # -1 to indicate no trigger
+
+            Try {
+                # If the task has a triger, overwrite the trigger vars with valid values
+                $Start         = $Task.Triggers | Select -ExpandProperty StartBoundary
+                $StartTime     = $Start.ToString("hh:mm")
+                $DaysOfWeek    = $Task.Triggers | Select -Expandproperty DaysOfWeek       #$Task.Triggers[0].DaysOfWeek 
+                $Repetition    = $Task.Triggers.Repetition | Select -ExpandProperty Interval
+                $Duration      = $Task.triggers.Repetition | Select -ExpandProperty Duration
+                $TriggerDays   = $Task.Triggers | Select -ExpandProperty DaysInterval  #$Task.Triggers[0].DaysInterval
+            } Catch {
+                Write-Host "--- INFO: No Trigger found for $Name"
+            }
+            
+            $splat = @{
+                'TaskPath'    = $TaskPath
+                'Name'        = $Name
+                'Author'      = $Author
+                'Description' = $Description
+                'Action'      = $Action
+                'Arguments'   = $Arguments
+                'Start'       = $Start
+                'StartTime'   = $StartTime
+                'Duration'    = $Duration
+                'Repetition'  = $Repetition
+                'DaysOfWeek'  = BinaryWeekdaysToString $DaysOfWeek
+                'TriggerDays' = If ($TriggerDays -eq 1) { "Daily" } else { $TriggerDays }
+                'Enabled'     = If ($Enabled) { "Enabled" } Else { "-" }
+            }
+
+            $obj = New-Object -TypeName PSObject -property $splat
+            $TasksResult += $obj
+        }
+        Catch {
+            Write-Host "ERROR: $Error"
+        }
     }
 }
+#------------------------------------------------------------------------------------------
+# Output
 
 # select specific columns
-$Result = $Result | Select-Object Name, Enabled, DaysOfWeek, TriggerDays, StartTime, Repetition, Duration, Action, Arguments, Description, Author, Start | 
-    Sort-Object Name
-$Result | Out-GridView
+$TasksResult = $TasksResult | Select-Object TaskPath, Name, Enabled, DaysOfWeek, TriggerDays, StartTime, Repetition, Duration, Action, Arguments, Description, Author, Start | 
+    Sort-Object TaskPath, Name
+$TasksResult | Out-GridView
 
 If ($SaveToCsv) {
-    $DateIso = Get-Date -Format "yyyy-MM-dd_hhmmss"
-    $SaveFullPath = [System.IO.Path]::Combine($CsvSaveFolder, "SchedTasks_$DateIso.csv")
-    $Result | Export-Csv $SaveFullPath -NoTypeInformation
+    $SaveFullPath = [System.IO.Path]::Combine($CsvSaveFolder, "SchedTasks.csv")
+    $TasksResult | Export-Csv $SaveFullPath -NoTypeInformation
 }
